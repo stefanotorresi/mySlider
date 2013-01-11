690253c9f4f6bfaae2fc5243946c7cdbbf2b9e07
Stefano Torresi <webdev@stefanotorresi.it>
2013-01-11 16:29:39 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,71 @@
+mySlider
+===
+
+Description
+---
+
+**mySlider** is a jQuery plugin designed to slide through absolutely positioned gargantuan background images inside a fixed height HTML block.
+
+Requirements
+---
+
+* jQuery 1.8.3 (it most likely works on older versions but I haven't tested it)
+
+Usage
+---
+
+### Html
+
+1. Add `slideshow-container` class to the block you want to add the background to.
+2. Append a child with `slideshow` class to the block.
+3. Put your slides blocks ( default class is `slide` ) inside the `slideshow` block;
+
+```html
+<div class="slideshow-container">
+	
+	<!-- your content here -->
+
+	<div class="slideshow">
+		<div class="slide">
+			<img src="slide1.jpg" alt="">
+		</div>
+		<div class="slide">
+			<img src="slide2.jpg" alt="">
+		</div>
+		<div class="slide">
+			<img src="slide3.jpg" alt="">
+		</div>
+	</div>
+</div>
+```
+
+### Javascript
+
+Just call the plugin on the `slideshow` element.
+
+```javascript
+$(".slideshow").mySlider();
+```
+
+You can pass the classic JSON to `mySlider()` with some options.
+They are quite self explanatory by looking at the defaults:
+
+```json
+{
+	interval :          5000,
+	speed :             1000,
+	slideSelector :     '.slide',
+	autoStart :         true,
+	controls:           true
+}
+```
+
+LICENSE
+--
+
+The files in this archive are released under the MIT license. You can find a copy of this license in LICENSE.txt.
+
+ACKNOWLEDGEMENTS
+--
+
+Most of the code is inspired by [Twitter Bootstrap's Carousel](http://twitter.github.com/bootstrap/javascript.html#carousel)
---
+++ jquery.myslider.js
@@ -0,0 +1,118 @@
+/**
+ * mySlider (http://github.com/stefanotorresi/myslider)
+ *
+ * Copyright (c) 2013 Stefano Torresi (http://stefanotorresi.it)
+ * See the file LICENSE.txt for copying permission.
+ * ************************************************
+ */
+
+(function($){
+    
+    var Slideshow = function (container, options) {
+        this.$container = $(container);
+        this.options = options;
+        this.cycling = false;
+        this.$slides = this.$container.children(this.options.slideSelector);
+        this.$current = this.$slides.first();
+        
+        if (this.options.controls) {
+            this.$container.append('<div class="buttons"><a class="prev" href="">prev</a><a class="next" href="">next</a></div>');
+        
+            this.$container.find('.buttons .next').click($.proxy(function(e){
+                this.next();
+                return false;
+            }, this));
+
+            this.$container.find('.buttons .prev').click($.proxy(function(e){
+                this.prev();
+                return false;
+            }, this));
+        }
+    }
+    
+    Slideshow.prototype = {
+        
+        play : function () {
+            this.cycling = true;
+
+            this.interval = setInterval($.proxy(function() { 
+                this.next();
+            }, this), this.options.interval );
+
+            return this;
+        },
+
+        pause: function() {
+            clearInterval(this.interval);
+            this.interval = null;
+
+            return this;
+        },
+
+        slideTo: function($slide) {                
+            this.pause();
+
+            $slide.stop(true).fadeTo(this.options.speed, 1, $.proxy(function(){
+
+                this.cycling && this.play();
+
+            }, this));
+
+            $slide.siblings(this.options.slideSelector).removeClass('active').stop(true).fadeTo(this.options.speed, 0);
+
+            $slide.addClass('active');
+
+            this.$current = $slide;
+
+            return this;                
+        },
+
+        next: function () {
+            var $next;
+
+            if (this.$current.next(this.options.slideSelector).length) {
+                $next = this.$current.next();
+            } else {
+                $next = this.$slides.first();
+            }
+
+            return this.slideTo($next);
+        },
+
+        prev: function () {                
+            var $prev;
+
+            if (this.$current.prev(this.options.slideSelector).length) {
+                $prev = this.$current.prev();
+            } else {
+                $prev = this.$slides.last();
+            }
+
+            return this.slideTo($prev);
+        }
+        
+    }
+    
+    $.fn.mySlider = function (options) {
+        
+        options = $.extend({
+            interval :          5000,
+            speed :             1000,
+            slideSelector :     '.slide',
+            autoStart :         true,
+            controls:           true
+        }, options);
+        
+        this.each(function(){
+            var slideshow = new Slideshow(this, options);
+            $(this).data('slideshow', slideshow);
+            if (options.autoStart) {
+                slideshow.play();
+            }
+        });
+        
+        return this;
+        
+    }
+    
+})(jQuery)
